63fd0969ee1deb8180968a90572d7421ec370aa2
Karen N. Black <blackkar@oregonstate.edu>
2021-07-05 15:25:59 -0500

    route update

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 require('dotenv').config();
 PORT = process.env.PORT || 5860;
 const db = require('./dbcon.js');
 const cors = require('cors');
 const bodyParser = require('body-parser');  // don't need? bodyparser is depracated
 const mysql = require('mysql');    // don't need because in dbcon.js file
-const path = require('path');
+
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true,}));
+app.use(cors());
+
+app.use('/menuItems', require('./menuItems.js'));
+app.use('/productCategories', require('./productCategories.js'));
+app.use('/carts', require('./carts.js'));
+app.use('/cartItems', require('./cartItems.js'));
+app.use('/payments', require('./payments.js'));
+
+
 app.use(express.static(path.join(__dirname, '/servesup/build')));
 app.use(express.static('public'));
 /*To handle routing*/
@@ -14,19 +27,6 @@ app.get('/*', (req, res) => {
   res.sendFile(path.join(__dirname, '/servesup/build', 'index.html'));
 });
 
-
-app.use(express.json());
-app.use(express.urlencoded({extended: true,}));
-app.use(cors());
-
-
-// app.use('/static', express.static('public'));  // not sure about this one for home page
-// app.use('/', express.static('public'));        // not sure about this one for home page
-app.use('/menuItems', require('./menuItems.js'));
-app.use('/productCategories', require('./productCategories.js'));
-app.use('/carts', require('./carts.js'));
-app.use('/cartItems', require('./cartItems.js'));
-app.use('/payments', require('./payments.js'));
 
 
 /* Error handler middleware */
